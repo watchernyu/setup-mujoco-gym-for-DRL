da66662a28b82e180822d0a9b7b1319e758e3c8a
watchernyu <cw1681@nyu.edu>
2018-07-14 14:33:43 +0800

    Update sample_hpc_script.sh

--- sample_hpc_script.sh
+++ sample_hpc_script.sh
@@ -3,8 +3,8 @@
 #SBATCH --verbose
 #SBATCH -p aquila 
 #SBATCH --job-name=test_job
-#SBATCH --output=hpclogs/test_job_%j.out
-#SBATCH --error=hpclogs/test_job_%j.err
+#SBATCH --output=test_job_%j.out
+#SBATCH --error=test_job_%j.err
 #SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=16GB
